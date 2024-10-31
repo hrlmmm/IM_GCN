4218c831d7002176a16c1d38a8f7970b2d2b2a8c
hrlmmm <huangys0228@163.com>
2024-10-31 22:25:50 +0800

    更改

--- net_generator.py
+++ net_generator.py
@@ -9,10 +9,10 @@ for i in n:
         BAgraph = nx.random_graphs.barabasi_albert_graph(i, j)  # 节点数，平均度数
         nx.write_edgelist(BAgraph, '/data/Train_'+str(i)+'_'+str(j), data=False)
 
-测试BA网络生成
-n = [10000]
-k = [2, 4, 8]
-for i in n:
-    for j in k:
-        BAgraph = nx.random_graphs.barabasi_albert_graph(i, j)  # 节点数，平均度数
-        nx.write_edgelist(BAgraph, 'Test_'+str(i)+'_'+str(j), data=False)
+# 测试BA网络生成
+# n = [10000]
+# k = [2, 4, 8]
+# for i in n:
+#     for j in k:
+#         BAgraph = nx.random_graphs.barabasi_albert_graph(i, j)  # 节点数，平均度数
+#         nx.write_edgelist(BAgraph, 'Test_'+str(i)+'_'+str(j), data=False)
