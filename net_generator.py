75e9c4db944f6f8876ca72c8b3278f0f19ef9dee
chenminghao <527670091@qq.com>
2024-10-31 21:32:09 +0800

    更改

---
+++ net_generator.py
@@ -0,0 +1,18 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# 训练BA网络生成
+n = [1000]
+k = [2]
+for i in n:
+    for j in k:
+        BAgraph = nx.random_graphs.barabasi_albert_graph(i, j)  # 节点数，平均度数
+        nx.write_edgelist(BAgraph, '/data/Train_'+str(i)+'_'+str(j), data=False)
+
+# 测试BA网络生成
+# n = [10000]
+# k = [2, 4, 8]
+# for i in n:
+#     for j in k:
+#         BAgraph = nx.random_graphs.barabasi_albert_graph(i, j)  # 节点数，平均度数
+#         nx.write_edgelist(BAgraph, 'Test_'+str(i)+'_'+str(j), data=False)
